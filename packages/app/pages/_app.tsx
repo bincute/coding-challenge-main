17db31be84238cf01e98c6959da4eae7c506416a
GONGBIN <GONGBIN@kingsoft.com>
2022-12-01 12:18:05 -0800

    delete test code

--- packages/app/pages/_app.tsx
+++ packages/app/pages/_app.tsx
@@ -2,14 +2,7 @@ import { ChakraProvider } from '@chakra-ui/react';
 import { defaultTheme } from '@cosmology/react';
 
 function CosmologyApp({ Component, pageProps }) {
-  // Add this in node_modules/react-dom/index.js
-const React1 = require('react');
-
-// Add this in your component file
-require('react-dom');
-const React2 = require('react');
-console.log('version', React1 === React2);
-
+  
   return (
     <ChakraProvider theme={defaultTheme}>
       <Component {...pageProps} />
